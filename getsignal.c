16a613b3a8433c11324ed536521c1a30c4d5efb4
fer-almeida <fernando.almeida@me.com>
2014-06-29 20:53:31 -0300

    Update getsignal.c

--- getsignal.c
+++ getsignal.c
@@ -5,7 +5,9 @@
 #include <unistd.h>
 
 #define MAJOR_VERSION	1
-#define MINOR_VERSION	0
+#define MINOR_VERSION	1
+
+#define LOGERROR	1 //Exibe as informações de erro
 
 void sig_handler(int signo) {
 	switch(signo) {
@@ -33,12 +35,46 @@ void sig_handler(int signo) {
 }
 
 int main(void) {
-	if(signal(SIGINT, sig_handler) == SIG_ERR) printf("\nImpossivel iniciar o handler SIGINT\n");
-	if(signal(SIGSYS, sig_handler) == SIG_ERR) printf("\nImpossivel iniciar o handler SIGSYS\n");
-	if(signal(SIGHUP, sig_handler) == SIG_ERR) printf("\nImpossivel iniciar o handler SIGHUP\n");
-	if(signal(SIGTERM, sig_handler) == SIG_ERR) printf("\nImpossivel iniciar o handler SIGTERM\n");
-	if(signal(SIGUSR1, sig_handler) == SIG_ERR) printf("\nImpossivel iniciar o handler SIGUSR1\n");
-	if(signal(SIGUSR2, sig_handler) == SIG_ERR) printf("\nImpossivel iniciar o handler SIGUSR2\n");
+	char** sigtable = {
+		"SIGHUP",
+		"SIGINT",
+		"SIGQUIT",
+		"SIGILL",
+		"SIGTRAP/SIGABRT",
+		"SIGIOT",
+		"SIGBUS",
+		"SIGFPE",
+		"SIGKILL",
+		"SIGUSR1",
+		"SIGEVG",
+		"SIGUSR2",
+		"SIGPIPE",
+		"SIGALRM",
+		"SIGTERM",
+		"SIGKFLT/SIGCHLD",
+		"SIGCLT",
+		"SIGCONT",
+		"SIGSTOP",
+		"SIGTSTP",
+		"SIGTTIN",
+		"SIGTTOUT",
+		"SIGURG",
+		"SIGXCPU",
+		"SIGXFSZ",
+		"SIGVTALRM",
+		"SIGPROF",
+		"SIGWINCH/SIGIO",
+		"SIGPOLL",
+		"SIGPWR",
+		"SIGSYS"
+	}
+	int i;
+	
+	if(LOGERROR) printf("Iniciando handlers:\n");
+	
+	for(i = 1; i <= 31; i++) {
+		if(signal(i, sig_handler) == SIGERR) if(LOGERROR) printf("Erro ao iniciar handler %s\n", sigtable[i - 1]);
+	}
 	
 	printf("Programa principal rodando...\n");
 	
