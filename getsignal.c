dcd39fcc191ff2b6caa74b53e7e269628e1a2e04
fer-almeida <fernando.almeida@me.com>
2014-06-29 22:46:56 -0300

    Update getsignal.c

--- getsignal.c
+++ getsignal.c
@@ -6,7 +6,7 @@
 #define MAJOR_VERSION	1
 #define MINOR_VERSION	1
 
-#define LOGERROR	TRUE //Exibe as informações de erro
+#define LOGERROR	1 //Exibe as informações de erro
 
 char* sigtable[31] = {
 	"01 SIGHUP",
